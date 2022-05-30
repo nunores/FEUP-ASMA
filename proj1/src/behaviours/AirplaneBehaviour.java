15c0d3718cd95afcfe90cfc462362be3031d1274
Nuno Resende <n.resende2000@gmail.com>
2022-05-30 17:22:04 +0100

    Changed folders

---
+++ proj1/src/behaviours/AirplaneBehaviour.java
@@ -0,0 +1,124 @@
+package behaviours;
+
+import agents.AirplaneAgent;
+import agents.ControlTowerAgent;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import jade.proto.ContractNetResponder;
+import jade.wrapper.StaleProxyException;
+import proposals.AirplaneProposal;
+import proposals.ControlTowerProposal;
+import utils.Logger;
+import utils.Printer;
+
+import java.io.IOException;
+
+public class AirplaneBehaviour extends ContractNetResponder {
+
+    private final AirplaneAgent agent;
+
+    public AirplaneBehaviour(Agent a, MessageTemplate mt) {
+        super(a, mt);
+        this.agent = (AirplaneAgent) a;
+    }
+
+    private ACLMessage createProposal(ACLMessage cfp) throws Exception {
+        ACLMessage propose = cfp.createReply();
+        propose.setPerformative(ACLMessage.PROPOSE);
+
+        AirplaneProposal proposalArgs = new AirplaneProposal(agent.getTimeToLand(), agent.getSpaceRequiredToLand(),
+                calculateTimeLeftOfFuel(agent.getFuel(), agent.getFuelPerSecond()), calculateUrgency(agent.getTimeWaiting(), agent.getFlightType()), agent.isSos(), agent.getName(), agent.getTimeWaiting(), agent.getFlightType());
+
+        try {
+            propose.setContentObject(proposalArgs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return propose;
+    }
+
+    private int calculateUrgency(int timeWaiting, AirplaneAgent.FlightType flightType) throws Exception {
+        int importanceDegree;
+        switch (flightType) {
+            case lowCost:
+                importanceDegree = 1;
+                break;
+            case vip:
+                importanceDegree = 5;
+                break;
+            case businessClass:
+                importanceDegree = 2;
+                break;
+            case _private:
+                importanceDegree = 3;
+                break;
+            default:
+                throw new Exception("No information about the flight type.");
+        }
+        return importanceDegree*(timeWaiting+1);
+    }
+
+    private int calculateTimeLeftOfFuel(int fuel, double fuelPerSecond) { return (int) (fuel / fuelPerSecond); }
+
+    @Override
+    protected ACLMessage handleCfp(ACLMessage cfp) throws RefuseException {
+        ControlTowerProposal proposal = null;
+        try {
+            proposal = (ControlTowerProposal) cfp.getContentObject();
+        } catch (UnreadableException e) {
+            e.printStackTrace();
+        }
+        assert proposal != null;
+        if (agent.getSpaceRequiredToLand() > proposal.getRunwayLength()) {
+            ACLMessage refuse = cfp.createReply();
+            refuse.setPerformative(ACLMessage.REFUSE);
+            throw new RefuseException(refuse);
+        } else {
+            try {
+                return createProposal(cfp);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    @Override
+    protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) {
+        ControlTowerAgent.landPlane(agent.getSpaceRequiredToLand());
+        try {
+            Logger.getInstance().writeToLog(agent.getName() + " is landing (Space to land: " + agent.getSpaceRequiredToLand() + " Time to land: " + agent.getTimeToLand() + " SOS: " + agent.isSos() + ")");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Printer.getInstance().printData();
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            Logger.getInstance().writeToLog((agent.getName() + " has finished landing (Space freed: " + agent.getSpaceRequiredToLand() + ")"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        ControlTowerAgent.parkPlane(agent.getSpaceRequiredToLand());
+                    }
+                },
+                agent.getTimeToLand() * 1000L
+        );
+
+        ACLMessage inform = accept.createReply();
+        inform.setPerformative(ACLMessage.INFORM);
+        try {
+            inform.setContentObject(agent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return inform;
+    }
+}
---
+++ proj1/src/behaviours/ControlTowerBehaviour.java
@@ -0,0 +1,127 @@
+package behaviours;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import jade.proto.ContractNetInitiator;
+
+import agents.ControlTowerAgent;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+import proposals.AirplaneProposal;
+import proposals.ControlTowerProposal;
+import utils.Logger;
+import utils.Runway;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
+public class ControlTowerBehaviour extends ContractNetInitiator {
+
+    private final ControlTowerAgent controlTowerAgent;
+
+    public ControlTowerBehaviour(Agent a, ACLMessage cfp) {
+        super(a, cfp);
+        this.controlTowerAgent = (ControlTowerAgent) a;
+    }
+
+    @Override
+    protected void handleInform(ACLMessage inform) {
+        ACLMessage message = new ACLMessage(ACLMessage.CFP);
+        ArrayList<AgentController> airplaneAgents = controlTowerAgent.getLaunchAgents().getAirplaneAgents();
+
+        for (AgentController agent : airplaneAgents) {
+            try {
+                message.addReceiver(new AID(agent.getName(), AID.ISGUID));
+            } catch (StaleProxyException e) {
+                e.printStackTrace();
+            }
+        }
+
+        message.setProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
+        message.setReplyByDate((new Date(System.currentTimeMillis() + 10000))); // 10 seconds reply limit
+
+        ControlTowerProposal proposal = new ControlTowerProposal(controlTowerAgent.getRunway().getOriginalLength());
+
+        try {
+            message.setContentObject(proposal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        controlTowerAgent.addBehaviour(new ControlTowerBehaviour(controlTowerAgent, message));
+    }
+
+    @Override
+    protected void handleAllResponses(Vector responses, Vector acceptances) {
+        List<AirplaneProposal> currentAirplanes = new ArrayList<>();
+        int bestValue = -1;
+        ACLMessage bestReply = null;
+        AirplaneProposal bestProposal = null;
+
+        for (int i = 0; i < responses.size(); i++) {
+            ACLMessage message = (ACLMessage) responses.get(i);
+            if (message.getPerformative() == ACLMessage.PROPOSE) {
+                ACLMessage reply = message.createReply();
+                reply.setPerformative(ACLMessage.REJECT_PROPOSAL);
+                acceptances.addElement(reply);
+
+                AirplaneProposal proposal = null;
+                try {
+                    proposal = (AirplaneProposal) message.getContentObject();
+                } catch (UnreadableException e) {
+                    e.printStackTrace();
+                }
+                currentAirplanes.add(proposal);
+                assert proposal != null;
+                int value = calculateValue(proposal);
+                if (value > bestValue) {
+                    bestValue = value;
+                    bestProposal = proposal;
+                    bestReply = reply;
+                }
+            }
+            else {
+                return;
+            }
+        }
+
+        currentAirplanes.remove(bestProposal);
+        controlTowerAgent.setCurrentAirplanes(currentAirplanes);
+
+        if (bestReply != null) {
+            bestReply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+        }
+        try {
+            Logger.getInstance().writeToLog(bestProposal.getName() + " will land next (Space to land: " + bestProposal.getSpaceRequiredToLand() + " SOS: " + bestProposal.isSos() + ")");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (!controlTowerAgent.enoughSpace(bestProposal.getSpaceRequiredToLand())) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            controlTowerAgent.getLaunchAgents().removeFromAirplaneAgents(bestProposal.getName());
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    private int calculateValue(AirplaneProposal proposal) {
+        if (proposal.isSos()) {
+            return 2147483647;
+        }
+        return proposal.getUrgency();
+    }
+}
